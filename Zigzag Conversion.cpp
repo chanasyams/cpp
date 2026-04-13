28e26a5b6ab1452e8c3115d640b9e7fa8faeab4e
chanasyams <259091346+chanasyams@users.noreply.github.com>
2026-04-13 11:47:35 +0530

    Add Zigzag Conversion solution in C++
    Implement Zigzag Conversion algorithm to rearrange characters in a string based on the specified number of rows.

---
+++ Zigzag Conversion.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    string convert(string s, int numRows) {
+ 
+        if (numRows <= 1 || numRows >= s.length()) return s;
+
+        vector<string> rows(numRows);
+        int currRow = 0;
+        bool goingDown = false;
+
+        for (char c : s) {
+            rows[currRow] += c;
+            
+
+            if (currRow == 0 || currRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+            
+            // Move up or down
+            currRow += goingDown ? 1 : -1;
+        }
+
+ 
+        string result = "";
+        for (const string& row : rows) {
+            result += row;
+        }
+
+        return result;
+    }
+};
+
