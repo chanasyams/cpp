88f31b9e3b46da531f884e18096841d3abfc39f2
chanasyams <259091346+chanasyams@users.noreply.github.com>
2026-04-13 12:27:36 +0530

    Create vector sort.py

---
+++ vector sort.py
@@ -0,0 +1,29 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n, temp;
+    vector<int> v;
+    
+    cin >> n;
+    
+
+    for(int i = 0; i < n; i++) {
+        cin >> temp;
+        v.push_back(temp);
+    }
+    
+
+    sort(v.begin(), v.end());
+    
+ 
+    for(int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
+    }
+    
+    return 0;
+}
